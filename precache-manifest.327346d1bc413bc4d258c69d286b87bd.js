7c8e51dcc53ceba7f3827e9f4245a944e8a3bf95
Mohamed <lahla81@yahoo.com>
2022-10-29 23:22:20 +0200

    Updates

---
+++ precache-manifest.327346d1bc413bc4d258c69d286b87bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "984339291aa79314ded9",
+    "url": "/healthydam/static/css/main.48f94438.chunk.css"
+  },
+  {
+    "revision": "984339291aa79314ded9",
+    "url": "/healthydam/static/js/main.7bda6eb4.chunk.js"
+  },
+  {
+    "revision": "82900612b88ac47c5300",
+    "url": "/healthydam/static/js/runtime~main.184bdfc0.js"
+  },
+  {
+    "revision": "d88ef17f8e5cf3f57801",
+    "url": "/healthydam/static/js/2.394572b2.chunk.js"
+  },
+  {
+    "revision": "5f3d73bccbd387d7c3c8f8ed329cdb47",
+    "url": "/healthydam/static/media/BigelowRules-Regular.5f3d73bc.ttf"
+  },
+  {
+    "revision": "e0118caaab9056e42002333cca2e5450",
+    "url": "/healthydam/index.html"
+  }
+];
